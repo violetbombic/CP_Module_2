b27c1bff383e63c77e8e2795ce0488015ad9a870
violetbombic <101185749+violetbombic@users.noreply.github.com>
2022-03-28 16:20:09 +0200

    Update Datamuse_API.py

--- Datamuse_API.py
+++ Datamuse_API.py
@@ -1,13 +1,8 @@
-import json,requests
-from pprint import pprint
 import streamlite as st 
 
-#keyword=input('plz give me a keyword: ')
-option = st.selectbox('What you want to choose?', ('rel_syn', 'rel_ant', 'sl’, 'ml'))
-st.write('You selected:', option):
-url= 'https://api.datamuse.com/words?rel_ant=' + keyword + '&max=10'
+option = st.selectbox(
+     'How would you like to be contacted?',
+     ('rel_syn', 'rel_ant', 'sl', 'ml', 'sd'))
 
-response = requests.get(url)   
-
-dataFromDatamuse = json.loads(response.text) 
+st.write('You selected:', option)
 
