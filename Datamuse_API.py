a021eef58ba2049fd5e015e4fd58b66625804487
violetbombic <101185749+violetbombic@users.noreply.github.com>
2022-03-28 19:04:51 +0200

    Update Datamuse_API.py

--- Datamuse_API.py
+++ Datamuse_API.py
@@ -1,38 +1,36 @@
 import streamlit as st 
 import json,requests
 
-keyword=st.text_input('plz give me a keyword: ')
+st.header('Datamuse Streamlit App')
 
-option = st.selectbox(
-     'Please choose an option: ',
+keyword=st.text_input('plz give me a keyword: ','')
+
+if keyword != '':
+     option = st.selectbox('Please choose an option: ',
      ('Synonymes', 'Antonyms', 'Sounds Like', 'Means Like', 'Spelled Like'))
 
-st.write('You selected:', option)
+     st.write('You selected:', option)
 
-if option == 'Synonymes':
-     url= 'https://api.datamuse.com/words?rel_syn=' + keyword + '&max=10'
-elif option == 'Antonyms':
-     url= 'https://api.datamuse.com/words?rel_ant=' + keyword + '&max=10'
-elif option == 'Sounds Like':
-     url= 'https://api.datamuse.com/words?sl=' + keyword + '&max=10'
-elif option == 'Means Like':
-     url= 'https://api.datamuse.com/words?ml=' + keyword + '&max=10'
-else:
-     url= 'https://api.datamuse.com/words?sd=' + keyword + '&max=10'
-     
-
-                                                   
-#url= 'https://api.datamuse.com/words?' + option + '=' + keyword + '&max=10'
-
-st.write("Here is the url: ", url)
-
-response = requests.get(url)   
-
-dataFromDatamuse = json.loads(response.text) 
-
-for eachentry in dataFromDatamuse:
-  st.write("--",eachentry['word'])
+     if option == 'Synonymes':
+          url= 'https://api.datamuse.com/words?rel_syn=' + keyword + '&max=10'
+     elif option == 'Antonyms':
+          url= 'https://api.datamuse.com/words?rel_ant=' + keyword + '&max=10'
+     elif option == 'Sounds Like':
+          url= 'https://api.datamuse.com/words?sl=' + keyword + '&max=10'
+     elif option == 'Means Like':
+          url= 'https://api.datamuse.com/words?ml=' + keyword + '&max=10'
+     else:
+          url= 'https://api.datamuse.com/words?sd=' + keyword + '&max=10'
 
 
-#st.write(dataFromDatamuse)
+     st.write("Here is the url: ", url)
+
+     response = requests.get(url)   
+
+     dataFromDatamuse = json.loads(response.text) 
+
+     for eachentry in dataFromDatamuse:
+          st.write("--",eachentry['word'])
+
+
 
