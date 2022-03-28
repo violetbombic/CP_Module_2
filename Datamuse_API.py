ab0c5c42a660878dbac7e9ebbf5cdd768af11b6b
violetbombic <101185749+violetbombic@users.noreply.github.com>
2022-03-28 18:47:44 +0200

    Update Datamuse_API.py

--- Datamuse_API.py
+++ Datamuse_API.py
@@ -1,18 +1,28 @@
 import streamlit as st 
-import json,requests
-from pprint import pprint
+
 
 keyword=st.text_input('plz give me a keyword: ')
 
 option = st.selectbox(
      'Please choose an option: ',
-     ('rel_syn', 'rel_ant', 'sl', 'ml', 'sd'))
+     ('Synonymes', 'Antonyms', 'Sounds Like', 'Means Like', 'Spelled Like'))
 
 st.write('You selected:', option)
 
+if option == 'Synonymes':
+     url= 'https://api.datamuse.com/words?rel_syn=' + keyword + '&max=10'
+elif option == 'Antonyms':
+     url= 'https://api.datamuse.com/words?rel_ant=' + keyword + '&max=10'
+elif option == 'Sounds Like':
+     url= 'https://api.datamuse.com/words?sl=' + keyword + '&max=10'
+elif option == 'Means Like':
+     url= 'https://api.datamuse.com/words?ml=' + keyword + '&max=10'
+else:
+     url= 'https://api.datamuse.com/words?sd=' + keyword + '&max=10'
+     
 
                                                    
-url= 'https://api.datamuse.com/words?' + option + '=' + keyword + '&max=10'
+#url= 'https://api.datamuse.com/words?' + option + '=' + keyword + '&max=10'
 
 st.write("Here is the url: ", url)
 
@@ -20,8 +30,10 @@ response = requests.get(url)
 
 dataFromDatamuse = json.loads(response.text) 
 
+sr.write("You chose: ", keyword, ". And the result of", 
+     
 for e in dataFromDatamuse:
-  st.write("You chose: ", keyword,". And the result of", option, "is",e['word'])
+  st.write("--",e['word'])
 
 
 #st.write(dataFromDatamuse)
