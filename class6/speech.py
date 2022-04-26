d8b4f9b524af83e09baa4c06af2b9dbdb761591b
violetbombic <101185749+violetbombic@users.noreply.github.com>
2022-04-26 12:28:50 +0200

    Update speech.py

--- class6/speech.py
+++ class6/speech.py
@@ -1,1 +1,13 @@
+import streamlit as st
+from gtts import gTTS
+import IPython.display as ipd  
 
+text_by_user = st.text_input("Please type in some text here: ")
+lang_code = st.text_input("Please type in 2-letter language code, for example: 'en' for english, 'de' for german...").lower()
+
+tts=gTTS(text= text_by_user, lang= lang_code)
+tts.save(text_by_user + '.mp3')
+
+audio_file = open(text_by_user + ".mp3", "rb")
+
+st.audio(data=audio_file, format="audio/mp3", start_time=0)
