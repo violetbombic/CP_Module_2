383682f37be51dcc474d0b66408a68a04589b1c2
violetbombic <101185749+violetbombic@users.noreply.github.com>
2022-04-26 12:55:52 +0200

    Update speech.py

--- class6/speech.py
+++ class6/speech.py
@@ -3,7 +3,7 @@ from gtts import gTTS
 import IPython.display as ipd  
 
 tts1=gTTS(text='hello world', lang='it')
-tts1.save('helloworld_italian.mp3')
+tts1.save('audio.mp3')
 
 audio_file = open("audio.mp3", "rb")
 st.write("you can listen to the audio here")
