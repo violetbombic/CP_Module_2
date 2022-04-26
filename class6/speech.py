7cde336d0087f57be1c0130eeea9d38277540e55
violetbombic <101185749+violetbombic@users.noreply.github.com>
2022-04-26 13:00:21 +0200

    Update speech.py

--- class6/speech.py
+++ class6/speech.py
@@ -9,3 +9,14 @@ audio_file = open("audio.mp3", "rb")
 st.write("you can listen to the audio here")
 
 st.audio(data=audio_file, format="audio/mp3", start_time=0)
+
+
+
+text_by_user = st.text_input("Please type in some text here: ")
+lang_code = st.text_input("Please type in 2-letter language code, for example: 'en' for english, 'de' for german...").lower()
+
+tts=gTTS(text= text_by_user, lang= lang_code)
+tts.save(text_by_user + '.mp3')
+
+
+st.audio(text_by_user, format="audio/wav", start_time=0)
