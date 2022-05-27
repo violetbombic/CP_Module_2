009c0361a56c80053b60f1b1a20643097ae48071
violetbombic <101185749+violetbombic@users.noreply.github.com>
2022-05-27 10:16:03 +0200

    Update final_project.py

--- FINALPROJECT/final_project.py
+++ FINALPROJECT/final_project.py
@@ -1,1 +1,9 @@
-
+import streamlit as st
+import re
+import PyPDF2
+import nltk
+from PyPDF2 import PdfFileReader
+from nltk import word_tokenize, sent_tokenize
+from nltk.corpus import stopwords 
+from string import punctuation
+from googletrans import Translator
